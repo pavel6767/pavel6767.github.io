13f79a9a63df831d7cd34110b4d69c49262c9492
Pavel Machuca <pavelmachuca@hotmail.com>
2019-02-28 01:59:52 -0500

    added transition JS animation, missing finetuning

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+
+// smooth scrolling with JQuery
 $("#nav-bar a").click(function () {
   var id = $(this).attr('href');
 
@@ -5,3 +7,25 @@ $("#nav-bar a").click(function () {
     scrollTop: $(id).offset().top - $('#nav-bar').height()
   }, 500);
 });
+
+
+// cross fade info text
+
+let intro = document.getElementById('intro-text');
+let textSlides = intro.getElementsByTagName('h3');
+
+let current = 0;
+
+setInterval(function () {
+  for (let i = 0; i < textSlides.length; i++) {
+    textSlides[i].style.opacity = 1;
+  }
+
+  if (textSlides.length % current === 0) {
+    current = 0;
+  } else {
+    current++;
+  }
+  textSlides[current].style.opacity = 1;
+
+}, 2000);
