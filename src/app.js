008357e721ffc9e8336c4df848b6ddb45aeaa019
pavel6767 <pavelmachuca@hotmail.com>
2019-05-02 02:19:05 -0400

    removed icons

--- src/app.js
+++ src/app.js
@@ -9,39 +9,3 @@ $("#nav-bar a").click(function () {
 });
 
 
-// cross fade info text
-
-function introFade() {
-  const content = [
-    "I love learning and implementing tools to assemble solutions",
-    "I'm a Full Stack Developer specializing in NERDS stack"
-  ]
-
-  let text = document.querySelector('#intro-text > h3');
-  let current = 0;
-
-  setInterval(function () {
-    text.textContent = content[current];
-
-    if (current > 0) {
-      current = 0;
-    } else {
-      current++;
-    }
-  }, 3000);
-}
-introFade()
-
-// setInterval(function () {
-//   for (let i = 0; i < con.length; i++) {
-//     textSlides[i].style.opacity = 1;
-//   }
-
-//   if (textSlides.length % current === 0) {
-//     current = 0;
-//   } else {
-//     current++;
-//   }
-//   textSlides[current].style.opacity = 1;
-
-// }, 2000);
